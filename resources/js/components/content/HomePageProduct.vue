d380100c9f259a26bd19bb015192b61a91c0595f
Khánh Vinh <du.vinh@mylangroup.com>
2023-02-06 13:03:21 +0700

    update slide

--- resources/js/components/content/HomePageProduct.vue
+++ resources/js/components/content/HomePageProduct.vue
@@ -1,50 +1,94 @@
 <template>
-    <div style="width: 100%; display: flex; justify-content: center;">
-        <div v-for="item in 5">
-        <div class="home__page-product">
-            <div class="product__item">           
-            </div>
-            <div class="product-list__size">
-                <div class="product-size__item">X</div>
-                <div class="product-size__item">XL</div>
-                <div class="product-size__item">XXL</div>
-                <div class="product-size__item">S</div>
-            </div>
-            <div data-collection-tag="" class="product-grid__content">
-                <div class="product-grid__options">
-                    <div data-option-id="color" class="options-color">
-                        <div rel-script="option-color-change" data-value="xam-nhat"
-                            data-images="[{&quot;id&quot;:&quot;06f10c28&quot;,&quot;src&quot;:&quot;\/image\/September2022\/jogger-casual-xam-nhat2_60.jpg&quot;},{&quot;id&quot;:&quot;a7200cb6&quot;,&quot;src&quot;:&quot;\/image\/October2022\/quan-ni-jogger-casual-xam.jpg&quot;},{&quot;id&quot;:&quot;39787160&quot;,&quot;src&quot;:&quot;\/image\/September2022\/jogger-casual-xam-nhat3_84.jpg&quot;},{&quot;id&quot;:&quot;b30007ed&quot;,&quot;src&quot;:&quot;\/image\/September2022\/jogger-casual-xam-nhat4_86.jpg&quot;},{&quot;id&quot;:&quot;d0de774a&quot;,&quot;src&quot;:&quot;\/image\/September2022\/jogger-casual-xam-nhat5_36.jpg&quot;},{&quot;id&quot;:&quot;6b0a3b59&quot;,&quot;src&quot;:&quot;\/image\/September2022\/jogger-casual-xam-nhat1_22.jpg&quot;},{&quot;id&quot;:&quot;0f8289b7&quot;,&quot;src&quot;:&quot;\/image\/October2022\/quan-jogger-casual-xam.jpg&quot;}]"
-                            class="option-color__item"><span class="checkmark"
-                                style="background-image: url(&quot;https://media.coolmate.me/cdn-cgi/image/width=160,height=160,quality=80,format=auto/uploads/October2022/quan-jogger-casual-xam.jpg&quot;);"></span>
+    <div>
+        <VueSlickCarousel :arrows="true" :dots="true" v-bind="setting">
+            <div v-for="item in 10" style="width: 100%; display: flex; justify-content: center;">
+                <div class="home__page-product">
+                    <div class="product__item">
+                    </div>
+                    <div class="product-list__size">
+                        <div class="product-size__item">X</div>
+                        <div class="product-size__item">XL</div>
+                        <div class="product-size__item">XXL</div>
+                        <div class="product-size__item">S</div>
+                    </div>
+                    <div data-collection-tag="" class="product-grid__content">
+                        <div class="product-grid__options">
+                            <div data-option-id="color" class="options-color">
+                                <div rel-script="option-color-change" data-value="xam-nhat"
+                                    data-images="[{&quot;id&quot;:&quot;06f10c28&quot;,&quot;src&quot;:&quot;\/image\/September2022\/jogger-casual-xam-nhat2_60.jpg&quot;},{&quot;id&quot;:&quot;a7200cb6&quot;,&quot;src&quot;:&quot;\/image\/October2022\/quan-ni-jogger-casual-xam.jpg&quot;},{&quot;id&quot;:&quot;39787160&quot;,&quot;src&quot;:&quot;\/image\/September2022\/jogger-casual-xam-nhat3_84.jpg&quot;},{&quot;id&quot;:&quot;b30007ed&quot;,&quot;src&quot;:&quot;\/image\/September2022\/jogger-casual-xam-nhat4_86.jpg&quot;},{&quot;id&quot;:&quot;d0de774a&quot;,&quot;src&quot;:&quot;\/image\/September2022\/jogger-casual-xam-nhat5_36.jpg&quot;},{&quot;id&quot;:&quot;6b0a3b59&quot;,&quot;src&quot;:&quot;\/image\/September2022\/jogger-casual-xam-nhat1_22.jpg&quot;},{&quot;id&quot;:&quot;0f8289b7&quot;,&quot;src&quot;:&quot;\/image\/October2022\/quan-jogger-casual-xam.jpg&quot;}]"
+                                    class="option-color__item"><span class="checkmark"
+                                        style="background-image: url(&quot;https://media.coolmate.me/cdn-cgi/image/width=160,height=160,quality=80,format=auto/uploads/October2022/quan-jogger-casual-xam.jpg&quot;);"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h3 class="product-grid__title"><a
+                                href="https://www.coolmate.me/product/quan-ni-nam-jogger-casual-co-gian?itm_source=home-highlight-product"
+                                rel-script="product-title" tabindex="0">
+                                Quần nỉ nam Jogger Casual co giãn
+                            </a></h3>
+                        <div class="product-grid__prices">
+                            <div rel-script="product-price" data-price="299000" data-compare-price="299000" data-sale="1"
+                                class="product-prices"><span style="color: red; margin-left: 10px;">-27%</span>
+                                <del>299.000đ</del>
+                                <ins>219.000đ </ins>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <h3 class="product-grid__title"><a
-                        href="https://www.coolmate.me/product/quan-ni-nam-jogger-casual-co-gian?itm_source=home-highlight-product"
-                        rel-script="product-title" tabindex="0">
-                        Quần nỉ nam Jogger Casual co giãn
-                    </a></h3>
-                <div class="product-grid__prices">
-                    <div rel-script="product-price" data-price="299000" data-compare-price="299000" data-sale="1"
-                        class="product-prices"><span style="color: red; margin-left: 10px;">-27%</span> <del>299.000đ</del>
-                        <ins>219.000đ </ins></div>
-                </div>
             </div>
-             </div>
-        </div>
-
+        </VueSlickCarousel>
+        
     </div>
+  
    
   
 </template>
 
 <script>
-import { VueperSlides, VueperSlide } from 'vueperslides'
-import 'vueperslides/dist/vueperslides.css'
+  import VueSlickCarousel from 'vue-slick-carousel'
+  // optional style for arrows & dots
+  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 export default {
     components:{
-        VueperSlides, VueperSlide
+        VueSlickCarousel
+    },
+    data(){
+        return{
+            setting:{
+  "dots": true,
+  "infinite": false,
+  "speed": 500,
+  "slidesToShow": 4,
+  "slidesToScroll": 4,
+  "initialSlide": 0,
+  "responsive": [
+    {
+      "breakpoint": 1024,
+      "settings": {
+        "slidesToShow": 3,
+        "slidesToScroll": 3,
+        "infinite": true,
+        "dots": true
+      }
+    },
+    {
+      "breakpoint": 600,
+      "settings": {
+        "slidesToShow": 2,
+        "slidesToScroll": 2,
+        "initialSlide": 2
+      }
+    },
+    {
+      "breakpoint": 480,
+      "settings": {
+        "slidesToShow": 1,
+        "slidesToScroll": 1
+      }
+    }
+  ]
+}
+        }
     }
 }
 </script>
